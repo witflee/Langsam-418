1ca40614503d5709004bc256609c3f2d450c68e0
Elias Weitfle <113947042+witflee@users.noreply.github.com>
2026-04-26 00:37:43 -0400

    add interaction text to mii

--- Assets/Resources/Models/Character/Scripts/CharacterInteraction.cs
+++ Assets/Resources/Models/Character/Scripts/CharacterInteraction.cs
@@ -1,14 +1,39 @@
 using UnityEngine;
+using TMPro;
+using System.Collections;
 
 public class CharacterInteraction : MonoBehaviour
 {
     public Animator animator;
-    //public GameObject textBubble;
+    public TextMeshPro dialogueText; 
+    public string dialogueMessage = "Hello!";
+    public float displayDuration = 3f;
+
+    private Coroutine _hideCoroutine;
 
     public void Interact()
     {
         animator.SetTrigger("Talk");
-        //textBubble.SetActive(true);
         Debug.Log("Hello");
+
+        ShowText();
+    }
+
+    private void ShowText()
+    {
+        // If already showing, cancel the previous hide timer
+        if (_hideCoroutine != null)
+            StopCoroutine(_hideCoroutine);
+
+        dialogueText.text = dialogueMessage;
+        dialogueText.gameObject.SetActive(true);
+
+        _hideCoroutine = StartCoroutine(HideAfterDelay());
+    }
+
+    private IEnumerator HideAfterDelay()
+    {
+        yield return new WaitForSeconds(displayDuration);
+        dialogueText.gameObject.SetActive(false);
     }
 }
